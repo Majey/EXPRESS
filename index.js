cb33d518415d6047c63b36619b9cb836176bd1d6
Majey <kevinerick78@yahoo.com>
2022-03-07 18:24:51 +0300

    Load HTML pages using Express

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const express = require("express");
+const path = require("path");
+
+const publicPath = path.join(__dirname, `public`);
+console.log(publicPath);
 
 // express app
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello, welcome!");
+    // res.send(`Hello, welcome!. My name is ${req.query.name} and I am ${req.query.age} years old`);
+    res.sendFile(`${publicPath}/index.html`);
 });
 
 app.get("/contacts", (req, res) => {
-    res.send("Here is our contacts");
+    // res.send("Here is our contacts");
+    res.sendFile(`${publicPath}/contacts.html`);
 });
 
 app.get("/about", (req, res) => {
-    res.send("About US")
+    // res.send("About US")
+    res.sendFile(`${publicPath}/about.html`);
 })
 
 app.listen(3000);
